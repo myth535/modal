28601af06858a56d9693b29a2824f9bc5003103e
Adrián Martínez Pérez <skyweb535@gmail.com>
2020-07-03 01:01:54 -0400

    Add redux logic

--- src/components/EstimationPoint.js
+++ src/components/EstimationPoint.js
@@ -1,10 +1,27 @@
 import React from 'react'
 import { View, Text, StyleSheet, Platform } from 'react-native'
+import store from '../redux/store'
 
 export default class EstimationPoint extends React.Component {
     constructor(props) {
         super(props)
+
+        this.state = {
+            isSmallScreen: store.getState().isSmallScreen,
+            unsubscribe: store.subscribe(this.updateState),
+        }
     }
+
+    updateState = () => {
+        this.setState({
+            isSmallScreen: store.getState().isSmallScreen,
+        })
+    }
+
+    componentWillUnmount() {
+        this.state.unsubscribe()
+    }
+
     render() {
         return <View></View>
     }

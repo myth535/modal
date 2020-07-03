9a74052d5a2e356d1e52476c383a440e3f367c26
Adrián Martínez Pérez <skyweb535@gmail.com>
2020-07-02 23:58:22 -0400

    Create action to update modal visibility

--- src/redux/actions.js
+++ src/redux/actions.js
@@ -1,8 +1,16 @@
 export const UPDATE_POINT_ESTIMATION = 'UPDATE_POINT_ESTIMATION'
+export const UPDATE_MODAL_VISIBILITY = 'UPDATE_MODAL_VISIBILITY'
 
 export function updatePointEstimation(pointEstimation) {
     return {
         type: UPDATE_POINT_ESTIMATION,
         pointEstimation,
+    }
+}
+
+export function updateModalVisibility(modalVisibility) {
+    return {
+        type: UPDATE_MODAL_VISIBILITY,
+        modalVisibility,
     }
 }

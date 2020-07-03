6065d2222322ba05873dd9d770daeec6f3cacf8d
Adrián Martínez Pérez <skyweb535@gmail.com>
2020-07-03 00:20:21 -0400

    Add redux logic

--- src/components/StoryPointEstimation.js
+++ src/components/StoryPointEstimation.js
@@ -1,9 +1,31 @@
 import React from 'react'
 import { View, StyleSheet, Animated } from 'react-native'
 
+import { updateModalVisibility, setResponsiveEvent } from '../redux/actions'
+import store from '../redux/store'
+
 class StoryPointEstimation extends React.Component {
     constructor(props) {
         super(props)
+
+        const globalState = store.getState()
+        this.state = {
+            modalVisibility: globalState.modalVisibility,
+            isSmallScreen: globalState.isSmallScreen,
+            unsubscribe: store.subscribe(this.updateState),
+        }
+    }
+
+    updateState = () => {
+        const globalState = store.getState()
+        this.setState({
+            modalVisibility: globalState.modalVisibility,
+            isSmallScreen: globalState.isSmallScreen,
+        })
+    }
+
+    componentWillUnmount() {
+        this.state.unsubscribe()
     }
 
     render() {

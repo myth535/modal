63b85f8f525badfded72ecb59a7e5695b7102118
Adrián Martínez Pérez <skyweb535@gmail.com>
2020-07-03 00:30:38 -0400

    Add redux logic

--- src/components/StoryPointModal.js
+++ src/components/StoryPointModal.js
@@ -1,14 +1,30 @@
 import React from 'react'
 import { View, StyleSheet, Text, Animated, Platform } from 'react-native'
 
+import store from '../redux/store'
+
 class StoryPointModal extends React.Component {
     constructor(props) {
         super(props)
+
+        this.state = {
+            isSmallScreen: store.getState().isSmallScreen,
+            unsubscribe: store.subscribe(this.updateState),
+        }
+    }
+
+    componentWillUnmount() {
+        this.state.unsubscribe()
+    }
+
+    updateState = () => {
+        this.setState({
+            isSmallScreen: store.getState().isSmallScreen,
+        })
     }
 
     render() {
         return <Animated.View></Animated.View>
     }
 }
-
 export default StoryPointModal

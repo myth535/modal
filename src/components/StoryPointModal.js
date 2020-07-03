840a7701909c7da34f9194973e1a449579cf3680
Adrián Martínez Pérez <skyweb535@gmail.com>
2020-07-03 00:29:36 -0400

    Create initial StoryPointModal component

---
+++ src/components/StoryPointModal.js
@@ -0,0 +1,14 @@
+import React from 'react'
+import { View, StyleSheet, Text, Animated, Platform } from 'react-native'
+
+class StoryPointModal extends React.Component {
+    constructor(props) {
+        super(props)
+    }
+
+    render() {
+        return <Animated.View></Animated.View>
+    }
+}
+
+export default StoryPointModal

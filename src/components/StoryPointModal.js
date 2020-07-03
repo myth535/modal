8ffb779e92933eeb3bdb600b3fa14f3052de5905
Adrián Martínez Pérez <skyweb535@gmail.com>
2020-07-03 00:33:24 -0400

    Create estimation points data

--- src/components/StoryPointModal.js
+++ src/components/StoryPointModal.js
@@ -3,6 +3,17 @@ import { View, StyleSheet, Text, Animated, Platform } from 'react-native'
 
 import store from '../redux/store'
 
+const estimationPointsData = [
+    { point: 0, time: '10 min' },
+    { point: 1, time: '30 min' },
+    { point: 2, time: '1-2 hours' },
+    { point: 3, time: 'Half day' },
+    { point: 5, time: '1 day' },
+    { point: 8, time: '1.5 days' },
+    { point: 13, time: '2 days' },
+    { point: 21, time: '3 days' },
+]
+
 class StoryPointModal extends React.Component {
     constructor(props) {
         super(props)
@@ -27,4 +38,5 @@ class StoryPointModal extends React.Component {
         return <Animated.View></Animated.View>
     }
 }
+
 export default StoryPointModal

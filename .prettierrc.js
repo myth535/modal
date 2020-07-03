23cefdd9f923a7ba3c6f3c864218c3e240d2c0b5
Adrián Martínez Pérez <skyweb535@gmail.com>
2020-07-02 23:53:49 -0400

    Initial commit

---
+++ .prettierrc.js
@@ -0,0 +1,15 @@
+module.exports = {
+    printWidth: 80,
+    tabWidth: 4,
+    useTabs: false,
+    semi: false,
+    singleQuote: true,
+    quoteProps: 'as-needed',
+    jsxSingleQuote: false,
+    trailingComma: 'es5',
+    bracketSpacing: true,
+    jsxBracketSameLine: false,
+    arrowParens: 'avoid',
+    proseWrap: 'preserve',
+    endOfLine: 'lf',
+}

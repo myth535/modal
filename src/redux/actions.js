38e759d133c84ca5d0a80515d5f3523540383fdf
Adrián Martínez Pérez <skyweb535@gmail.com>
2020-07-02 23:58:44 -0400

    Create action to set a responsive event

--- src/redux/actions.js
+++ src/redux/actions.js
@@ -1,5 +1,6 @@
 export const UPDATE_POINT_ESTIMATION = 'UPDATE_POINT_ESTIMATION'
 export const UPDATE_MODAL_VISIBILITY = 'UPDATE_MODAL_VISIBILITY'
+export const SET_RESPONSIVE_EVENT = 'SET_RESPONSIVE_EVENT'
 
 export function updatePointEstimation(pointEstimation) {
     return {
@@ -12,5 +13,12 @@ export function updateModalVisibility(modalVisibility) {
     return {
         type: UPDATE_MODAL_VISIBILITY,
         modalVisibility,
+    }
+}
+
+export function setResponsiveEvent(isSmallScreen) {
+    return {
+        type: SET_RESPONSIVE_EVENT,
+        isSmallScreen,
     }
 }

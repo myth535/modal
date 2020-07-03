10a0cfade55e63e34e6a1fea8b94f56188913c81
Adrián Martínez Pérez <skyweb535@gmail.com>
2020-07-03 00:00:32 -0400

    Import action constants

--- src/redux/store.js
+++ src/redux/store.js
@@ -1,3 +1,9 @@
+import {
+    UPDATE_POINT_ESTIMATION,
+    UPDATE_MODAL_VISIBILITY,
+    SET_RESPONSIVE_EVENT,
+} from './actions'
+
 export const initialState = {
     pointEstimation: 5,
     modalVisibility: false,

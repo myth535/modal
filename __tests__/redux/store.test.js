0ce360d74511abac5b949061d98597cd357669a0
Adrián Martínez Pérez <skyweb535@gmail.com>
2020-07-03 00:07:20 -0400

    Add test cases for updating story points case in the reducer

--- __tests__/redux/store.test.js
+++ __tests__/redux/store.test.js
@@ -10,4 +10,16 @@ describe('Redux Reducers', () => {
         const state = reducer(undefined, {})
         expect(state).toEqual(initialState)
     })
+
+    it('should update the story points with 1', () => {
+        const state = reducer(undefined, updatePointEstimation(1))
+        const newState = { ...initialState, pointEstimation: 1 }
+        expect(state).toEqual(newState)
+    })
+
+    it('should update the story points with 5', () => {
+        const state = reducer(undefined, updatePointEstimation(5))
+        const newState = { ...initialState, pointEstimation: 5 }
+        expect(state).toEqual(newState)
+    })
 })

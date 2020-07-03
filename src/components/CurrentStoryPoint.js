13f21e0871325c0d82d35ee1ab2a0bdcd2915710
Adrián Martínez Pérez <skyweb535@gmail.com>
2020-07-03 00:12:51 -0400

    Add redux logic

--- src/components/CurrentStoryPoint.js
+++ src/components/CurrentStoryPoint.js
@@ -1,9 +1,25 @@
 import React from 'react'
 import { View, Text, StyleSheet, Platform } from 'react-native'
+import store from '../redux/store'
 
 export default class CurrentStoryPoint extends React.Component {
     constructor(props) {
         super(props)
+
+        this.state = {
+            pointEstimation: store.getState().pointEstimation,
+            unsubscribe: store.subscribe(this.updateState),
+        }
+    }
+
+    updateState = () => {
+        this.setState({
+            pointEstimation: store.getState().pointEstimation,
+        })
+    }
+
+    componentWillUnmount() {
+        this.state.unsubscribe()
     }
 
     render() {

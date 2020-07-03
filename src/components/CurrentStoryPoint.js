9d607d193760af2e89d6a140e109a8d14856ffa9
Adrián Martínez Pérez <skyweb535@gmail.com>
2020-07-03 00:58:27 -0400

    Complete layout of current story point component

--- src/components/CurrentStoryPoint.js
+++ src/components/CurrentStoryPoint.js
@@ -1,5 +1,6 @@
 import React from 'react'
 import { View, Text, StyleSheet, Platform } from 'react-native'
+import { updateModalVisibility } from '../redux/actions'
 import store from '../redux/store'
 
 export default class CurrentStoryPoint extends React.Component {
@@ -27,6 +28,56 @@ export default class CurrentStoryPoint extends React.Component {
     }
 
     render() {
-        return <View></View>
+        return (
+            <View
+                style={styles.buttonContainer}
+                onStartShouldSetResponder={this.setResponder}
+                onResponderRelease={this.showOptions}
+            >
+                <View style={styles.pointsContainer}>
+                    <Text style={styles.pointText}>
+                        {this.state.pointEstimation}
+                    </Text>
+                </View>
+                <Text style={styles.textLabel}>Points</Text>
+            </View>
+        )
     }
 }
+
+const styles = StyleSheet.create({
+    buttonContainer: {
+        flexDirection: 'row',
+        backgroundColor: '#E7ECEF',
+        borderRadius: 60,
+        width: Platform.OS === 'android' ? 80 : 60,
+        paddingHorizontal: 2,
+        padding: 3,
+        ...Platform.select({
+            web: {
+                cursor: 'pointer',
+            },
+        }),
+    },
+    pointsContainer: {
+        borderRadius: 60,
+        marginHorizontal: 5,
+        alignItems: 'center',
+        justifyContent: 'center',
+        width: Platform.OS === 'android' ? 22 : 13,
+        height: Platform.OS === 'android' ? 22 : 13,
+        borderWidth: 1,
+        borderColor: '#8A94A6',
+    },
+    pointText: {
+        color: '#8A94A6',
+        marginHorizontal: 5,
+        alignSelf: 'center',
+        fontSize: Platform.OS === 'android' ? 8 : 8,
+    },
+
+    textLabel: {
+        color: '#8A94A6',
+        fontSize: Platform.OS === 'android' ? 14 : 10,
+    },
+})

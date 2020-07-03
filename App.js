2697baafb607f21236d6978fc29f69ab1d9627e4
Adrián Martínez Pérez <skyweb535@gmail.com>
2020-07-03 00:38:06 -0400

    Show the entire component tree

--- App.js
+++ App.js
@@ -1,8 +1,8 @@
 import React from 'react'
-import { View } from 'react-native'
+import StoryPointEstimation from './src/components/StoryPointEstimation'
 
 export default class App extends React.Component {
     render() {
-        return <View></View>
+        return <StoryPointEstimation />
     }
 }

4931075394f4db7fb2ab0f64bb05a3c7ce079744
Adrián Martínez Pérez <skyweb535@gmail.com>
2020-07-03 01:01:11 -0400

    Create initial EstimationPoint component

---
+++ src/components/EstimationPoint.js
@@ -0,0 +1,11 @@
+import React from 'react'
+import { View, Text, StyleSheet, Platform } from 'react-native'
+
+export default class EstimationPoint extends React.Component {
+    constructor(props) {
+        super(props)
+    }
+    render() {
+        return <View></View>
+    }
+}

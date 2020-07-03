b95b581cffcbe0dab2a5f602c87b273471ba99c2
Adrián Martínez Pérez <skyweb535@gmail.com>
2020-07-03 00:15:04 -0400

    Create function to accept being a touch responder

--- src/components/CurrentStoryPoint.js
+++ src/components/CurrentStoryPoint.js
@@ -22,6 +22,10 @@ export default class CurrentStoryPoint extends React.Component {
         this.state.unsubscribe()
     }
 
+    setResponder() {
+        return true
+    }
+
     render() {
         return <View></View>
     }

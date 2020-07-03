7769291a83628c37aca52bddcf2140cf6620ffd6
Adrián Martínez Pérez <skyweb535@gmail.com>
2020-07-03 00:23:45 -0400

    React responsively to width changes in the layout

--- src/components/StoryPointEstimation.js
+++ src/components/StoryPointEstimation.js
@@ -4,6 +4,7 @@ import { View, StyleSheet, Animated } from 'react-native'
 import { updateModalVisibility, setResponsiveEvent } from '../redux/actions'
 import store from '../redux/store'
 
+const smallWidthBreakpoint = 408
 class StoryPointEstimation extends React.Component {
     constructor(props) {
         super(props)
@@ -28,8 +29,19 @@ class StoryPointEstimation extends React.Component {
         this.state.unsubscribe()
     }
 
+    onLayoutChange = event => {
+        const width = event.nativeEvent.layout.width
+        const { isSmallScreen } = this.state
+
+        if (width < smallWidthBreakpoint && !isSmallScreen) {
+            store.dispatch(setResponsiveEvent(true))
+        } else if (width > smallWidthBreakpoint && isSmallScreen) {
+            store.dispatch(setResponsiveEvent(false))
+        }
+    }
+
     render() {
-        return <View></View>
+        return <View onLayout={this.onLayoutChange}></View>
     }
 }
 

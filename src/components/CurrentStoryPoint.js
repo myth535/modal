518feae23f1ba9d9527496a2b9d84ec4c22d5560
Adrián Martínez Pérez <skyweb535@gmail.com>
2020-07-03 00:11:22 -0400

    Create initial CurrentStoryPoint component

---
+++ src/components/CurrentStoryPoint.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import { View, Text, StyleSheet, Platform } from 'react-native'
+
+export default class CurrentStoryPoint extends React.Component {
+    constructor(props) {
+        super(props)
+    }
+
+    render() {
+        return <View></View>
+    }
+}

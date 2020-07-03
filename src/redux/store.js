d7d73e5bca22981f51a3770001b2ac6811d04644
Adrián Martínez Pérez <skyweb535@gmail.com>
2020-07-03 00:01:24 -0400

    Create the redux reducer

--- src/redux/store.js
+++ src/redux/store.js
@@ -9,3 +9,25 @@ export const initialState = {
     modalVisibility: false,
     isSmallScreen: false,
 }
+
+export const reducer = (state = initialState, action) => {
+    switch (action.type) {
+        case UPDATE_POINT_ESTIMATION:
+            return {
+                ...state,
+                pointEstimation: action.pointEstimation,
+            }
+        case UPDATE_MODAL_VISIBILITY:
+            return {
+                ...state,
+                modalVisibility: action.modalVisibility,
+            }
+        case SET_RESPONSIVE_EVENT:
+            return {
+                ...state,
+                isSmallScreen: action.isSmallScreen,
+            }
+        default:
+            return state
+    }
+}

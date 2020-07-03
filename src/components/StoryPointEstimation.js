539c7906d6ef75e38c55910d16a16ac2f9da0985
Adrián Martínez Pérez <skyweb535@gmail.com>
2020-07-03 00:18:35 -0400

    Create initial StoryPointEstimation component

---
+++ src/components/StoryPointEstimation.js
@@ -0,0 +1,14 @@
+import React from 'react'
+import { View, StyleSheet, Animated } from 'react-native'
+
+class StoryPointEstimation extends React.Component {
+    constructor(props) {
+        super(props)
+    }
+
+    render() {
+        return <View></View>
+    }
+}
+
+export default StoryPointEstimation

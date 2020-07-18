121ff758678d83f25e0073a38e1f7e6e2723adce
Adrián Martínez Pérez <skyweb535@gmail.com>
2020-07-17 18:15:01 -0700

    Check the selected point at the beginning

--- __tests__/e2e/fullWorkflow.e2e.js
+++ __tests__/e2e/fullWorkflow.e2e.js
@@ -1,5 +1,10 @@
 describe('Modal E2E', () => {
     it('Recreating the entire workflow', () => {
         cy.visit('http://localhost:19006/');
+
+        let currentPoint = '5'     //The initial value in the redux store
+
+        //The current selected point must be 5
+        cy.get('div').eq(8).should('have.text', currentPoint)
     });
 });

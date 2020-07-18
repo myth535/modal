b071fd966fa6edf2a94fb2a0dd43e966e02a1ddb
Adrián Martínez Pérez <skyweb535@gmail.com>
2020-07-17 18:16:49 -0700

    Add function to check modal opening

--- __tests__/e2e/fullWorkflow.e2e.js
+++ __tests__/e2e/fullWorkflow.e2e.js
@@ -1,3 +1,18 @@
+function checkingOpenModalProperly(tree, currentPoint) {
+    //Click the current point button to open the modal
+    cy.get('div').eq(6).click()
+
+    //Validate a property in the modal (to know that it is open)
+    cy.get('div').eq(10).should('have.css', 'margin-top', '20px')
+
+    //Validate a header text in the modal (to know that it is open)
+    cy.get('div').eq(12).should('have.text', "Story Point Estimation")
+
+    //Validate that the selected point in the modal is the current point, the background color class r-backgroundColor-t6fudj, is only for the selected option
+    cy.get('div').eq(33).should('have.text', currentPoint)
+    cy.get('div').eq(33).should('have.class', 'r-backgroundColor-t6fudj')
+}
+
 describe('Modal E2E', () => {
     it('Recreating the entire workflow', () => {
         cy.visit('http://localhost:19006/');
